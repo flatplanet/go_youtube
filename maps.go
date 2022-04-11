5cb269ed6cbaf59550f26eb78edbf21da185be8f
John Elder <johne4196@gmail.com>
2022-04-11 09:06:14 -0700

    Maps

---
+++ maps.go
@@ -0,0 +1,37 @@
+package main
+
+// import other packages
+import (
+	"fmt"
+)
+
+
+func main(){
+
+	// Maps var pizza = map [key_type]value_type{...}
+	var pizza = map[string]string{
+		"John":"Pepperoni",
+		"Mary":"Cheese",
+		"Tim":"Mushroom"}
+
+	fmt.Println(pizza)
+	fmt.Println(pizza["John"])
+
+	// Shorthand
+	// toppings := map[string]strin{"John":"Pepperoni"}
+
+	// Change an Item
+	pizza["John"] = "Peppers"
+	fmt.Println(pizza["John"])
+
+	// Delete Items
+	delete(pizza, "Tim")
+	fmt.Println(pizza)
+	
+	// Iterate Over Maps
+	for key, value := range pizza {
+		fmt.Println(key, value)
+	}	
+
+
+}
